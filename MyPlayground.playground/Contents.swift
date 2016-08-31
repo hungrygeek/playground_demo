e441ffabb8d67d6f65c6bf62107466a5e920b057
Shi Shu <s.shu@wustl.edu>
2016-08-31 12:38:34 -0500

    initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var x = 2
+
+func hello(input:String) -> String {
+    return input + "aha"
+}
+
+hello("test")
